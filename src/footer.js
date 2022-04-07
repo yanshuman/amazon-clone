3c9942fd989c15baebd626378a242f8b05f84c33
assccii <assccii@outlook.com>
2022-04-07 16:20:15 +0530

    Amazon Clone

---
+++ src/footer.js
@@ -0,0 +1,82 @@
+import React from "react"
+export default function Footer() {
+    return (
+        <footer>
+            <nav className = "footer">
+                <nav className="footer-1">
+                    <h3>Get us to know</h3>
+                    <ul>
+                        <li>About us</li>
+                        <li>Careers</li>
+                        <li>Press Releases</li>
+                        <li>Amazon Cares</li>
+                        <li>Gift a smile</li>
+                        <li>Amazon Science</li>
+                    </ul>
+                </nav>
+
+                <nav className="footer-2">
+                <h3>Connect With us</h3>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                    <li>Instagram</li>
+                </ul>
+                </nav>
+
+                <nav className="footer-3">
+                    <h3>Make money with us</h3>
+                    <ul>
+                        <li>Sell on Amazon</li>
+                        <li>Sell under Amazon accelerator</li>
+                        <li>Amazon Global selling</li>
+                        <li>Become an affiliate</li>
+                        <li>Fulfillment by Amazon</li>
+                        <li>Advertise your product</li>
+                        <li>Amazon Pay on merchant</li>                        
+                    </ul>
+
+                </nav>
+                
+                <nav className="footer-4">
+                    <h3>Let us help you</h3>
+                        <ul>
+                            <li>Covid-19 and Amazon</li>
+                            <li>Your account</li>
+                            <li>Return center</li>
+                            <li>100% purchase protection</li>
+                            <li>Amazon app download</li>
+                            <li>Amazon assistant download</li>
+                            <li>Help</li>
+                        </ul>
+
+                </nav>
+            </nav>
+
+            <nav className="country">
+                <ul>
+                    <li>Australia</li>
+                    <li>Brazil</li>
+                    <li>Canada</li>
+                    <li>China</li>
+                    <li>France</li>
+                    <li>Germany</li>
+                    <li>Italy</li>
+                    <li>Japan</li>
+                    <li>Mexico</li>
+                    <li>Netherlands</li>
+                    <li>Poland</li>
+                    <li>Singapore</li>
+                    <li>Spain</li>
+                </ul>
+
+            </nav>
+
+            <nav className="team-name">
+                <h3>Made with <span>&#x2764;</span> by Team Never Lucky</h3>
+            </nav>
+
+
+        </footer>
+    )
+}
