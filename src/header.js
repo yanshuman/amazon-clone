9eee14b5e3edbaf33f83d9bf99691beed5dd28cd
Black-D3vil007 <singhatmagyan@outlook.com>
2022-04-10 11:58:33 +0530

    updated

--- src/header.js
+++ src/header.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import SearchIcon from "@material-ui/icons/Search";
-import "./header.css";
+import India from './image/india.png';
+import "./style/header.css";
 
 function Header() {
-    return(
-        <div className="header">
-            <img className="header_logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"  alt="amazon"/>
+    return ( 
+    <div className = "header" >
+        <img className = "header_logo" src = "https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt = "amazon" / >
 
-            <div className="header_Search">
-                <input type="text" className="header_searchInput" />
-                <SearchIcon className="header_searchIcon" />
-            </div>
+        <div className = "header_Search" >
+            <select>
+                <option value = "all" > All </option>   
+                <option value = "appliances" > Appliances </option>   
+                <option value = "clothings" > Clothing </option>   
+                <option value = "Beauty" > Beauty </option>   
+            </select>   
+
+            <input className="header_searchInput" type="text" />
+            <SearchIcon className = "header_searchIcon" / >
+        </div> 
+
+        <div className="language">
+            <img className="lang" src={India} alt="india" />
+            <select name = "dropdown">
+                <option value="english">English</option>
+                <option value="english">Hindi</option>
+            </select>
+        </div>
+        
         </div>
     );
 }
